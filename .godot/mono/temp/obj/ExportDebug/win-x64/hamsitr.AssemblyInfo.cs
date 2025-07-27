b11722f8f3a079aa42378192a59994b3c47e0e0e
Jeo0 <jjrelb@gmail.com>
2025-07-28 01:24:21 +0800

    export

--- .godot/mono/temp/obj/ExportDebug/win-x64/hamsitr.AssemblyInfo.cs
+++ .godot/mono/temp/obj/ExportDebug/win-x64/hamsitr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hamsitr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportDebug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a18e7dcb516f09af813af1d856cbbe94a429643")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d3f29a688444cba759803a695bb965b88446e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("hamsitr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hamsitr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
