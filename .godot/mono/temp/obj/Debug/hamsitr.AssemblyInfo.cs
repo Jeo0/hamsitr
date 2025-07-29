c438cb21eb089e53f97147c9de14d5a3dcd38a27
Jeo0 <jjrelb@gmail.com>
2025-07-30 04:58:21 +0800

    n

--- .godot/mono/temp/obj/Debug/hamsitr.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/hamsitr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hamsitr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a8b53ebd9a5d5fca618c27e51decf43920285d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f50b1be18588b0bdb77aff07e05aec56175aeb15")]
 [assembly: System.Reflection.AssemblyProductAttribute("hamsitr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hamsitr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- scripts/player/states/sittingState.gd
+++ scripts/player/states/sittingState.gd
@@ -11,7 +11,7 @@ func enter() -> void:
 	
 var e_current_state: GOTO_STATES = GOTO_STATES.GOTO_DEFAULT
 const e_decay: float = 0.5;
-const e_max_tickle_accumulation: float = 8000;
+const e_max_tickle_accumulation: float = 19000;
 const e_max_pet_accumulation: float = 79222;
 
 var e_orig_scale: Vector2 = Vector2(0.195, 0.195)
@@ -20,10 +20,64 @@ enum GOTO_STATES{
 	GOTO_DEFAULT,
 	GOTO_DECAY,
 	GOTO_TRANSITION,
+	GOTO_PET_SEMI,
 	GOTO_PET_A,
 	GOTO_PET_B,
 }
+func update(delta: float) -> void:
+	# Tickle accumulation always grows with velocity
+	if player.g_tickle_accumulation < e_max_tickle_accumulation:
+		player.g_tickle_accumulation += Input.get_last_mouse_velocity().length()
 
+	var mouse_speed = Input.get_last_mouse_velocity().length()
+
+	print("accum: " + str(player.g_tickle_accumulation).pad_decimals(3)
+			+ "\t\ttimeleft: " + str(player.local_timer.time_left).pad_decimals(3)
+			+ "\t\tcurrent accum: " + str(Input.get_last_mouse_velocity().length()).pad_decimals(3)
+			+ "\t\tstate: giggle")
+			
+	# Allow state transition logic only if not in a PET state
+	match e_current_state:
+		GOTO_STATES.GOTO_DEFAULT, GOTO_STATES.GOTO_DECAY, GOTO_STATES.GOTO_TRANSITION:
+			if mouse_speed < player.g_tickle_threshold:
+				e_current_state = GOTO_STATES.GOTO_DECAY
+			else:
+				e_current_state = GOTO_STATES.GOTO_TRANSITION
+
+	# Process current state
+	match e_current_state:
+		GOTO_STATES.GOTO_DEFAULT:
+			player.animation_sprite.play("sitting_default")
+			player.g_tickle_accumulation = e_max_tickle_accumulation
+			player.local_timer.stop()
+
+		GOTO_STATES.GOTO_DECAY:
+			_invoke_start_timer(player.local_timer, 0.4)
+			if player.local_timer.time_left <= 0.1:
+				player.local_timer.start(0.09)
+				player.g_tickle_accumulation *= e_decay
+				player.animation_sprite.play_backwards("sitting_transition")
+
+			if player.g_tickle_accumulation <= 1.0:
+				e_current_state = GOTO_STATES.GOTO_DEFAULT
+
+		GOTO_STATES.GOTO_TRANSITION:
+			player.animation_sprite.play("sitting_transition")
+
+		GOTO_STATES.GOTO_PET_SEMI:
+			player.animation_sprite.play("sitting_semi_pet")
+			if player.g_tickle_accumulation > e_max_tickle_accumulation:
+				e_current_state = _pet_a_or_b()
+
+		GOTO_STATES.GOTO_PET_A:
+			player.animation_sprite.play("sitting_petting_A")
+
+		GOTO_STATES.GOTO_PET_B:
+			player.animation_sprite.play("sitting_petting_B")
+			if player.g_tickle_accumulation < e_max_tickle_accumulation:
+				e_current_state = GOTO_STATES.GOTO_DECAY
+
+"""
 func update(delta: float) -> void:
 	######################################
 	## tickle
@@ -54,29 +108,62 @@ func update(delta: float) -> void:
 				player.g_tickle_accumulation *= e_decay;
 				player.animation_sprite.play_backwards("sitting_transition")
 			
+			# go back to default once the accumulation keeps descending
 			if player.g_tickle_accumulation <= 1.0:
-				e_current_state = GOTO_STATES.GOTO_TRANSITION
+				e_current_state = GOTO_STATES.GOTO_DEFAULT
 				#player._change_state(load("res://scripts/player/states/petHoldState.gd").new())
 				
+				
 		GOTO_STATES.GOTO_TRANSITION:
-			pass
+			player.animation_sprite.play("sitting_transition")
+		
+		
+		GOTO_STATES.GOTO_PET_SEMI:
+			# logic of the tickling
+			player.animation_sprite.play("sitting_semi_pet")
+			
+			if player.g_tickle_accumulation > e_max_tickle_accumulation:
+				e_current_state = _pet_a_or_b()
+			
 		
 		GOTO_STATES.GOTO_PET_A:
-			pass
+			player.animation_sprite.play("sitting_petting_A")
+			
+
 
 		GOTO_STATES.GOTO_PET_B:
-			pass
+			player.animation_sprite.play("sitting_petting_B")
+			
+			# proceed to decay once we descend further than e_max_tickle_accumu
+			if player.g_tickle_accumulation < e_max_tickle_accumulation:
+				e_current_state = GOTO_STATES.GOTO_DECAY
+			
+			
+"""
 
+func _pet_a_or_b() -> GOTO_STATES:
+	if randf() > 0.5: # 50.001% up to 100%
+		return GOTO_STATES.GOTO_PET_A
+	return GOTO_STATES.GOTO_PET_B
+	
 	
 func _invoke_start_timer(timer, m_time_seconds) -> void:
 	if timer.is_stopped() == true: 
 		timer.start(m_time_seconds)
+		
 
 func handle_input(event) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
 		print("\n=======\ngraabiingg dis fkr\n=======");
 		player._change_state(load("res://scripts/player/states/grabbingState.gd").new())
 
+	
+	
+func on_animation_finished() -> void:
+	# after animation finished on transition
+	# proceed to set current state to semi
+	if e_current_state == GOTO_STATES.GOTO_TRANSITION:
+		e_current_state = GOTO_STATES.GOTO_PET_SEMI
 	
 	
 func on_azy_animation_sprite_animation_finished() -> void:
