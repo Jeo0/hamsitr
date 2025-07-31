5c4041f8882637d818932efe8f1308862f039fa8
Jeo0 <jjrelb@gmail.com>
2025-08-01 00:17:27 +0800

    bug fix using subwindows

--- .godot/mono/temp/obj/Debug/hamsitr.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/hamsitr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hamsitr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47eb8489aa603f4c7298d163410157f6f27419f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc515e8ca2f9fe47428aad1497a9bae4a7a3e902")]
 [assembly: System.Reflection.AssemblyProductAttribute("hamsitr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hamsitr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
